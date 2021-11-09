01e6daa614136e2a08fd05c279f470519da9a9be
dikuznet85@gmail.com <dikuznet85@gmail.com>
2021-11-09 21:32:16 +0500

    Leetcode Reverse Integer

--- 0python-demo/7leetcode/2string.py
+++ 0python-demo/7leetcode/2string.py
@@ -8,6 +8,14 @@ class Solution:
         s.reverse()
         return s
 
+    def reverse(self, x: int) -> int:
+        l = list(str(x).replace("-",""))
+        l.reverse()
+        x = -1 * int(''.join(l)) if x <0 else int(''.join(l))
+        if (x < -1*(2**31)) or (x >= (2**31 - 1)): x = 0 
+        return x
+
 sol = Solution()
 s = ["h","e","l","l","o"]
-print(sol.reverseString(s))
+print(sol.reverse(-2147483648))
+print(-1*(2**31))
