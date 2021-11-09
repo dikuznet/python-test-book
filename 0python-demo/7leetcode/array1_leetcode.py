77d7f544e9e964493b034cfe3f6681c87029ec82
dkuznetsov <dkuznetsov@micont.ru>
2021-11-09 08:50:28 +0500

    Formatter

--- 0python-demo/7leetcode/array1_leetcode.py
+++ 0python-demo/7leetcode/array1_leetcode.py
@@ -4,6 +4,7 @@ import time
 from functools import reduce
 import operator
 
+
 def benchmark(func):
     def wrapper(*args, **kwargs):
         start = time.monotonic()
@@ -13,56 +14,62 @@ def benchmark(func):
         return return_value
     return wrapper
 
+
 class Solution:
     @benchmark
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)
 
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits)-1,-1,-1): 
-            if digits[i] + 1 > 9: digits[i] = 0
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] + 1 > 9:
+                digits[i] = 0
             else:
                 digits[i] = digits[i] + 1
-                break   
-        if digits[0]==0: digits.insert(0, 1)  
-        return digits 
-    @benchmark    
+                break
+        if digits[0] == 0:
+            digits.insert(0, 1)
+        return digits
+
+    @benchmark
     def moveZeroes(self, nums: List[int]) -> None:
         zcount = 0
         l = len(nums)
-        i = 0 
-        while True: 
-            if i >= l: break 
-            if nums[i] == 0: 
+        i = 0
+        while True:
+            if i >= l:
+                break
+            if nums[i] == 0:
                 nums.insert(l, nums.pop(i))
                 zcount = zcount + 1
-                if i + zcount >= l: break
-            else:              
-                i = i + 1   
+                if i + zcount >= l:
+                    break
+            else:
+                i = i + 1
         return nums
 
     def singleNumber(self, nums: List[int]) -> int:
         setn = set()
         once = set()
         for item in nums:
-            if not (item in setn): 
+            if not (item in setn):
                 once.add(item)
-            else: 
+            else:
                 once.remove(item)
             setn.add(item)
         return once.pop()
 
     def singleNumber2(self, nums: List[int]) -> int:
         return reduce(operator.xor, nums)
-    
+
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         c = (set(nums1) & set(nums2))
         ret = list()
-        for i in c: 
-            ret = ret + [i] * min(nums1.count(i),nums2.count(i))
+        for i in c:
+            ret = ret + [i] * min(nums1.count(i), nums2.count(i))
         return ret
 
-    def robot(self,prices, days):
+    def robot(self, prices, days):
         profit = 0
         for i in range(1, days):
             if prices[i] > prices[i-1]:
@@ -71,7 +78,8 @@ class Solution:
 
     def maxProfit(self, prices: List[int]) -> int:
         profit = self.robot(prices, len(prices))
-        return profit,prices
-            
+        return profit
+
+
 x = Solution()
-print(x.maxProfit([7,6,4,3,1]))
+print(x.maxProfit([7, 6, 4, 3, 1]))
