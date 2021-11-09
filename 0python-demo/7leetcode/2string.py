fb532a9a853fe8c55c401d66ab14611ecc1f964f
dikuznet85@gmail.com <dikuznet85@gmail.com>
2021-11-09 21:51:58 +0500

    Remove comment

--- 0python-demo/7leetcode/2string.py
+++ 0python-demo/7leetcode/2string.py
@@ -2,9 +2,6 @@ from typing import List
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
         s.reverse()
         return s
 
