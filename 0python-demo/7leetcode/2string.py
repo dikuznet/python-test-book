d46d68501dc3ba544d982c6acf9b80adbb18cf7d
dkuznetsov <dkuznetsov@micont.ru>
2021-11-10 09:51:53 +0500

    Check PEP8

--- 0python-demo/7leetcode/2string.py
+++ 0python-demo/7leetcode/2string.py
@@ -1,34 +1,41 @@
 from typing import List
 from lutils import benchmark
+
+
 class Solution:
-    def reverseString(self, s:List[str]) -> None:
+
+    def reverseString(self, s: List[str]) -> None:
         s.reverse()
         return s
 
     def reverse(self, x: int) -> int:
-        l = list(str(x).replace("-",""))
+        l = list(str(x).replace("-", ""))
         l.reverse()
-        x = -1 * int(''.join(l)) if x <0 else int(''.join(l))
-        if (x < -1*(2**31)) or (x >= (2**31 - 1)): x = 0 
+        x = -1 * int(''.join(l)) if x < 0 else int(''.join(l))
+        if (x < -1*(2**31)) or (x >= (2**31 - 1)):
+            x = 0
         return x
-    
+
     @benchmark
     def firstUniqChar(self, s: str) -> int:
-        for i,val in enumerate(s): 
+        for i, val in enumerate(s):
             c = s.count(val)
-            if c==1: return i    
+            if c == 1:
+                return i
         return -1
 
     @benchmark
-    def firstUniqChar_fast(self, s: str) -> int:
+    def firstUniqCharFast(self, s: str) -> int:
         letters = 'abcdefghijklmnopqrstuvwxyz'
         idx = {}
-        for i,val in enumerate(letters): 
+        for i, val in enumerate(letters):
             c = s.count(val)
-            if c==1: 
+            if c == 1:
                 idx[s.index(val)] = val
-        if len(idx)>0: return min(idx.keys())
+        if len(idx) > 0:
+            return min(idx.keys())
         return -1
+
 
 sol = Solution()
 
