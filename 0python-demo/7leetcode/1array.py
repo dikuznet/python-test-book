8fb873976587b24bbc6a78c6453f2743f8e2720b
dkuznetsov <dkuznetsov@micont.ru>
2021-11-10 09:40:07 +0500

    Leetcode FirstUniqChar

--- 0python-demo/7leetcode/1array.py
+++ 0python-demo/7leetcode/1array.py
@@ -4,16 +4,7 @@ import time
 from functools import reduce
 import operator
 import numpy as np
-
-def benchmark(func):
-    def wrapper(*args, **kwargs):
-        start = time.monotonic()
-        return_value = func(*args, **kwargs)
-        end = time.monotonic()
-        print(f'Время выполнения: {end-start} секунд.')
-        return return_value
-    return wrapper
-
+from lutils import benchmark
 
 class Solution:
     @benchmark
--- 0python-demo/7leetcode/2string.py
+++ 0python-demo/7leetcode/2string.py
@@ -1,7 +1,7 @@
 from typing import List
-
+from lutils import benchmark
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
+    def reverseString(self, s:List[str]) -> None:
         s.reverse()
         return s
 
@@ -11,8 +11,25 @@ class Solution:
         x = -1 * int(''.join(l)) if x <0 else int(''.join(l))
         if (x < -1*(2**31)) or (x >= (2**31 - 1)): x = 0 
         return x
+    
+    @benchmark
+    def firstUniqChar(self, s: str) -> int:
+        for i,val in enumerate(s): 
+            c = s.count(val)
+            if c==1: return i    
+        return -1
+
+    @benchmark
+    def firstUniqChar_fast(self, s: str) -> int:
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        idx = {}
+        for i,val in enumerate(letters): 
+            c = s.count(val)
+            if c==1: 
+                idx[s.index(val)] = val
+        if len(idx)>0: return min(idx.keys())
+        return -1
 
 sol = Solution()
-s = ["h","e","l","l","o"]
-print(sol.reverse(-2147483648))
-print(-1*(2**31))
+
+print(sol.firstUniqChar_fast("leetcode"))
